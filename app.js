955b182582fd490896e36e0e4f4ddd8abd12edf7
xudi04 <129602928+xudi04@users.noreply.github.com>
2023-06-23 22:55:31 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended : true}));
 app.use(express.static('public'));
